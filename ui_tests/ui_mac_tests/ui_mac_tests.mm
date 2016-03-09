cc12def7e63ae5db4513b59b49200e704be95973
Yuki Yasoshima <yukiyasos@gmail.com>
2016-03-09 21:02:09 +0900

    add test project

---
+++ ui_tests/ui_mac_tests/ui_mac_tests.mm
@@ -0,0 +1,21 @@
+//
+//  ui_mac_tests.m
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ui_mac_tests : XCTestCase
+
+@end
+
+@implementation ui_mac_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
