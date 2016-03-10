40e2db3c698b8a73a0f6191f9e19b2b99173d0cf
Yuki Yasoshima <yukiyasos@gmail.com>
2016-03-10 22:38:11 +0900

    add transforms

---
+++ ui/yas_ui_matrix.h
@@ -0,0 +1,19 @@
+//
+//  yas_ui_matrix.h
+//
+
+#pragma once
+
+#include <simd/simd.h>
+
+namespace yas {
+namespace ui {
+    namespace matrix {
+        simd::float4x4 scale(float const x, float const y);
+        simd::float4x4 translation(float const x, float const y);
+        simd::float4x4 rotation(float const degree);
+        simd::float4x4 ortho(float const left, float const right, float const bottom, float const top, float const near,
+                             float const far);
+    }
+}
+}
