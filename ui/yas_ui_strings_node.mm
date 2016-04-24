392e75cad0a81f831a871a2a0705ea6d271e13ea
Yuki Yasoshima <yukiyasos@gmail.com>
2016-04-24 12:17:32 +0900

    fix strings_node word_size

--- ui/yas_ui_strings_node.mm
+++ ui/yas_ui_strings_node.mm
@@ -16,7 +16,7 @@ struct ui::strings_node::impl : base::impl {
     using super_class = base::impl;
 
     impl(ui::font_atlas &&str_data, std::size_t const max_word_count)
-        : _square_node(make_square_node(str_data.words().size())), _font_atlas(std::move(str_data)) {
+        : _square_node(make_square_node(max_word_count)), _font_atlas(std::move(str_data)) {
         _square_node.node().mesh().set_texture(_font_atlas.texture());
         _update_mesh_data();
     }
