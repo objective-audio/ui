a9c4216b37fba7265eb779554c9f843244e5f8e6
Yuki Yasoshima <yuki@MacBook-12.local>
2016-04-29 21:17:53 +0900

    add collision detector

---
+++ ui/yas_ui_collision_detector.h
@@ -0,0 +1,41 @@
+//
+//  yas_ui_collision_detector.h
+//
+
+#pragma once
+
+#include <simd/simd.h>
+#include "yas_base.h"
+#include "yas_protocol.h"
+
+namespace yas {
+namespace ui {
+    class collider;
+
+    struct updatable_collision_detector : protocol {
+        struct impl : protocol::impl {
+            virtual void clear_colliders() = 0;
+            virtual void push_front_collider(ui::collider &&) = 0;
+        };
+
+        explicit updatable_collision_detector(std::shared_ptr<impl>);
+
+        void clear_colliders();
+        void push_front_collider(ui::collider);
+    };
+
+    class collision_detector : public base {
+       public:
+        collision_detector();
+        collision_detector(std::nullptr_t);
+
+        ui::collider detect(simd::float2 const &);
+        bool detect(simd::float2 const &, ui::collider const &);
+
+        updatable_collision_detector updatable();
+
+       private:
+        class impl;
+    };
+}
+}
