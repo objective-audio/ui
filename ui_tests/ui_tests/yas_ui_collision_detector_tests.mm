0eb6f6cd3078ce1d3fcc4d8ec6c9bda9823721ee
Yuki Yasoshima <yukiyasos@gmail.com>
2016-04-30 11:11:05 +0900

    add detector tests

--- ui_tests/ui_tests/yas_ui_collision_detector_tests.mm
+++ ui_tests/ui_tests/yas_ui_collision_detector_tests.mm
@@ -3,6 +3,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "yas_ui_collider.h"
 #import "yas_ui_collision_detector.h"
 
 using namespace yas;
@@ -32,6 +33,37 @@ using namespace yas;
     ui::collision_detector detector{nullptr};
 
     XCTAssertFalse(detector);
+}
+
+- (void)test_detect {
+    ui::collision_detector detector;
+
+    XCTAssertFalse(detector.detect(0.0f));
+
+    ui::collider collider1{{.shape = ui::collider_shape::square}};
+    ui::collider collider2{{.shape = ui::collider_shape::square}};
+
+    detector.updatable().push_front_collider(collider1);
+    detector.updatable().push_front_collider(collider2);
+
+    XCTAssertEqual(detector.detect(0.0f), collider2);
+
+    detector.updatable().clear_colliders();
+
+    XCTAssertFalse(detector.detect(0.0f));
+}
+
+- (void)test_detect_with_collider {
+    ui::collision_detector detector;
+
+    ui::collider collider1{{.shape = ui::collider_shape::square}};
+    ui::collider collider2{{.shape = ui::collider_shape::square}};
+
+    detector.updatable().push_front_collider(collider1);
+    detector.updatable().push_front_collider(collider2);
+
+    XCTAssertFalse(detector.detect(0.0f, collider1));
+    XCTAssertTrue(detector.detect(0.0f, collider2));
 }
 
 @end
