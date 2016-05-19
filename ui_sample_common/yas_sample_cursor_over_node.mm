0c45dfaf79d82ba8705804e27dcec6a6d90607e7
Yuki Yasoshima <yukiyasos@gmail.com>
2016-05-19 20:50:17 +0900

    fix sample

--- ui_sample_common/yas_sample_cursor_over_node.mm
+++ ui_sample_common/yas_sample_cursor_over_node.mm
@@ -46,7 +46,7 @@ struct sample::cursor_over_node::impl : base::impl {
             node.set_position({100.0f, 0.0f});
             node.set_scale({10.0f, 30.0f});
             node.set_color(0.3f);
-            node.set_collider({{.shape = ui::collider_shape::square}});
+            node.set_collider(ui::collider{{.shape = ui::collider_shape::square}});
 
             ui::node handle_node;
             handle_node.push_back_sub_node(node);
