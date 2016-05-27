804a4929895ecbf2988190f6fe61b97628e71ca3
Yuki Yasoshima <yukiyasos@gmail.com>
2016-05-28 00:08:17 +0900

    add batch tests

---
+++ ui_tests/ui_tests/yas_ui_batch_tests.mm
@@ -0,0 +1,43 @@
+//
+//  yas_ui_batch_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_ui_batch.h"
+#import "yas_ui_batch_protocol.h"
+#import "yas_ui_node.h"
+
+using namespace yas;
+
+@interface yas_ui_batch_tests : XCTestCase
+
+@end
+
+@implementation yas_ui_batch_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_create {
+    ui::batch batch;
+
+    XCTAssertTrue(batch);
+
+    XCTAssertTrue(batch.root_node());
+    XCTAssertTrue(batch.render_node());
+
+    XCTAssertTrue(batch.renderable());
+}
+
+- (void)test_create_null {
+    ui::batch batch{nullptr};
+
+    XCTAssertFalse(batch);
+}
+
+@end
