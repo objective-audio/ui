1e32e927cd29eec7a352c6d5c3b9bca4fe70286d
Yuki Yasoshima <yukiyasos@gmail.com>
2016-05-28 16:10:14 +0900

    add metal_render_encoder files

---
+++ ui/yas_ui_render_encoder_protocol.mm
@@ -0,0 +1,19 @@
+//
+//  yas_ui_render_encoder_protocol.mm
+//
+
+#include "yas_ui_mesh.h"
+#include "yas_ui_render_encoder_protocol.h"
+#include "yas_ui_renderer.h"
+
+using namespace yas;
+
+ui::render_encodable::render_encodable(std::shared_ptr<impl> impl) : protocol(std::move(impl)) {
+}
+
+ui::render_encodable::render_encodable(std::nullptr_t) : protocol(nullptr) {
+}
+
+void ui::render_encodable::push_back_mesh(ui::mesh mesh) {
+    impl_ptr<impl>()->push_back_mesh(std::move(mesh));
+}
