df172791ce6b52eb2c447bcaedf5be4312d8a3b4
Yuki Yasoshima <yukiyasos@gmail.com>
2016-06-05 12:16:47 +0900

    rename image impl variables

--- ui/yas_ui_image.cpp
+++ ui/yas_ui_image.cpp
@@ -9,40 +9,40 @@ using namespace yas;
 
 struct ui::image::impl : base::impl {
     impl(uint_size const point_size, double const scale_factor)
-        : point_size(point_size),
-          scale_factor(scale_factor),
-          actual_size(uint_size{static_cast<uint32_t>(point_size.width * scale_factor),
-                                static_cast<uint32_t>(point_size.height * scale_factor)}) {
+        : _point_size(point_size),
+          _scale_factor(scale_factor),
+          _actual_size(uint_size{static_cast<uint32_t>(point_size.width * scale_factor),
+                                 static_cast<uint32_t>(point_size.height * scale_factor)}) {
         CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
         CGBitmapInfo bitmapInfo = static_cast<CGBitmapInfo>(kCGImageAlphaPremultipliedLast);
-        bitmap_context = CGBitmapContextCreate(NULL, actual_size.width, actual_size.height, 8, actual_size.width * 4,
-                                               colorSpace, bitmapInfo);
+        _bitmap_context = CGBitmapContextCreate(NULL, _actual_size.width, _actual_size.height, 8,
+                                                _actual_size.width * 4, colorSpace, bitmapInfo);
         CGColorSpaceRelease(colorSpace);
     }
 
     ~impl() {
-        CGContextRelease(bitmap_context);
+        CGContextRelease(_bitmap_context);
     }
 
     void clear() {
-        CGContextClearRect(bitmap_context, CGRectMake(0, 0, actual_size.width, actual_size.height));
+        CGContextClearRect(_bitmap_context, CGRectMake(0, 0, _actual_size.width, _actual_size.height));
     }
 
     void draw(std::function<void(CGContextRef const)> const &function) {
-        CGContextSaveGState(bitmap_context);
+        CGContextSaveGState(_bitmap_context);
 
-        CGContextTranslateCTM(bitmap_context, 0.0, actual_size.height);
-        CGContextScaleCTM(bitmap_context, (CGFloat)actual_size.width / (CGFloat)point_size.width,
-                          -(CGFloat)actual_size.height / (CGFloat)point_size.height);
-        function(bitmap_context);
+        CGContextTranslateCTM(_bitmap_context, 0.0, _actual_size.height);
+        CGContextScaleCTM(_bitmap_context, (CGFloat)_actual_size.width / (CGFloat)_point_size.width,
+                          -(CGFloat)_actual_size.height / (CGFloat)_point_size.height);
+        function(_bitmap_context);
 
-        CGContextRestoreGState(bitmap_context);
+        CGContextRestoreGState(_bitmap_context);
     }
 
-    uint_size point_size;
-    double scale_factor;
-    uint_size actual_size;
-    CGContextRef bitmap_context;
+    uint_size _point_size;
+    double _scale_factor;
+    uint_size _actual_size;
+    CGContextRef _bitmap_context;
 };
 
 ui::image::image(uint_size const point_size, double const scale_factor)
@@ -53,23 +53,23 @@ ui::image::image(std::nullptr_t) : base(nullptr) {
 }
 
 ui::uint_size ui::image::point_size() const {
-    return impl_ptr<impl>()->point_size;
+    return impl_ptr<impl>()->_point_size;
 }
 
 ui::uint_size ui::image::actual_size() const {
-    return impl_ptr<impl>()->actual_size;
+    return impl_ptr<impl>()->_actual_size;
 }
 
 double ui::image::scale_factor() const {
-    return impl_ptr<impl>()->scale_factor;
+    return impl_ptr<impl>()->_scale_factor;
 }
 
 const void *ui::image::data() const {
-    return CGBitmapContextGetData(impl_ptr<impl>()->bitmap_context);
+    return CGBitmapContextGetData(impl_ptr<impl>()->_bitmap_context);
 }
 
 void *ui::image::data() {
-    return CGBitmapContextGetData(impl_ptr<impl>()->bitmap_context);
+    return CGBitmapContextGetData(impl_ptr<impl>()->_bitmap_context);
 }
 
 void ui::image::clear() {
