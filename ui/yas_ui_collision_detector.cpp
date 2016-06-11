2e9ba172477e6ab2fd904fe28a5870c61804830c
Yuki Yasoshima <yukiyasos@gmail.com>
2016-06-11 11:42:42 +0900

    collider updates

--- ui/yas_ui_collision_detector.cpp
+++ ui/yas_ui_collision_detector.cpp
@@ -12,27 +12,28 @@ using namespace yas;
 
 struct ui::collision_detector::impl : base::impl, updatable_collision_detector::impl {
     impl() {
-        _updates.flags.set();
+        _updating = true;
     }
 
-    void set_updated(ui::collider_update_reason const reason) override {
-        _updates.set(reason);
+    bool is_updating() override {
+        return _updating;
     }
 
-    void clear_colliders_if_needed() override {
-        if (_needs_update(ui::collider_update_reason::existence)) {
-            _colliders.clear();
+    void begin_update() override {
+        _updating = true;
+        _colliders.clear();
+    }
+
+    void push_front_collider(ui::collider &&collider) override {
+        if (!_updating) {
+            throw "collision_detector is not updating.";
         }
+
+        _colliders.emplace_front(std::move(collider));
     }
 
-    void push_front_collider_if_needed(ui::collider &&collider) override {
-        if (_needs_update(ui::collider_update_reason::existence)) {
-            _colliders.emplace_front(std::move(collider));
-        }
-    }
-
-    void finalize() override {
-        _updates.flags.reset();
+    void end_update() override {
+        _updating = false;
     }
 
     ui::collider detect(ui::point const &location) {
@@ -55,11 +56,7 @@ struct ui::collision_detector::impl : base::impl, updatable_collision_detector::
 
    private:
     std::deque<ui::collider> _colliders;
-    ui::collider_updates_t _updates;
-
-    bool _needs_update(ui::collider_update_reason const reason) {
-        return _updates.test(reason);
-    }
+    bool _updating = false;
 };
 
 #pragma mark - ui::collision_detector
