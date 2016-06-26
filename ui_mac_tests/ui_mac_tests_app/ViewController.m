4a0431d1b32e69c4486cffa6980437c73d1c98ca
Yuki Yasoshima <yukiyasos@gmail.com>
2016-06-26 10:51:51 +0900

    update test project

---
+++ ui_mac_tests/ui_mac_tests_app/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  ui_mac_tests
+//
+//  Created by 八十嶋祐樹 on 2016/06/26.
+//  Copyright © 2016年 Yuki Yasoshima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
