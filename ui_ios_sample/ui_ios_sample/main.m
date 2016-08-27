b057608c98bb6adff939b9e3ba6b22440adb7b05
Yuki Yasoshima <yukiyasos@gmail.com>
2016-08-27 11:29:57 +0900

    fix comment

--- ui_ios_sample/ui_ios_sample/main.m
+++ ui_ios_sample/ui_ios_sample/main.m
@@ -1,15 +1,11 @@
 //
 //  main.m
-//  ui_ios_sample
-//
-//  Created by 八十嶋祐樹 on 2016/04/21.
-//  Copyright © 2016年 Yuki Yasoshima. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
