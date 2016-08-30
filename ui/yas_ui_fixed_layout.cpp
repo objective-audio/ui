51f963c394b40e2129b264222e0687c3e7ff56ec
Yuki Yasoshima <yukiyasos@gmail.com>
2016-08-30 10:54:48 +0900

    remove files

--- ui/yas_ui_fixed_layout.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  yas_ui_fixed_layout.cpp
-//
-
-#include "yas_ui_fixed_layout.h"
-#include "yas_ui_layout.h"
-
-using namespace yas;
--- ui/yas_ui_fixed_layout.h
+++
@@ -1,8 +0,0 @@
-//
-//  yas_ui_fixed_layout.h
-//
-
-#pragma once
-
-#include "yas_base.h"
-#include "yas_ui_layout_guide.h"
