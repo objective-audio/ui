7aa3062a3262deb1e8542392d59b9c702b50df09
Yuki Yasoshima <yukiyasos@gmail.com>
2017-11-07 00:03:40 +0900

    this

--- ui/yas_ui_metal_system.mm
+++ ui/yas_ui_metal_system.mm
@@ -32,11 +32,11 @@ struct ui::metal_system::impl : base::impl,
                                 renderable_metal_system::impl,
                                 testable_metal_system::impl {
     impl(id<MTLDevice> const device) : _device(device) {
-        _command_queue.move_object([device newCommandQueue]);
-        _default_library.move_object([device newDefaultLibrary]);
-        _inflight_semaphore.move_object(dispatch_semaphore_create(_uniforms_buffer_count));
+        this->_command_queue.move_object([device newCommandQueue]);
+        this->_default_library.move_object([device newDefaultLibrary]);
+        this->_inflight_semaphore.move_object(dispatch_semaphore_create(ui::_uniforms_buffer_count));
 
-        auto defaultLibrary = _default_library.object();
+        auto defaultLibrary = this->_default_library.object();
 
         auto fragment_program_with_texture =
             make_objc_ptr([defaultLibrary newFunctionWithName:@"fragment2d_with_texture"]);
@@ -65,38 +65,38 @@ struct ui::metal_system::impl : base::impl,
 
         auto pipeline_state_desc = make_objc_ptr([MTLRenderPipelineDescriptor new]);
         auto pipelineStateDesc = pipeline_state_desc.object();
-        pipelineStateDesc.sampleCount = _sample_count;
+        pipelineStateDesc.sampleCount = this->_sample_count;
         pipelineStateDesc.vertexFunction = vertexProgram;
         pipelineStateDesc.fragmentFunction = fragmentProgramWithTexture;
         [pipelineStateDesc.colorAttachments setObject:colorDesc atIndexedSubscript:0];
-        pipelineStateDesc.depthAttachmentPixelFormat = _depth_pixel_format;
-        pipelineStateDesc.stencilAttachmentPixelFormat = _stencil_pixel_format;
+        pipelineStateDesc.depthAttachmentPixelFormat = this->_depth_pixel_format;
+        pipelineStateDesc.stencilAttachmentPixelFormat = this->_stencil_pixel_format;
 
-        _multi_sample_pipeline_state_with_texture.move_object(
+        this->_multi_sample_pipeline_state_with_texture.move_object(
             [device newRenderPipelineStateWithDescriptor:pipelineStateDesc error:nil]);
 
         pipelineStateDesc.fragmentFunction = fragmentProgramWithoutTexture;
 
-        _multi_sample_pipeline_state_without_texture.move_object(
+        this->_multi_sample_pipeline_state_without_texture.move_object(
             [device newRenderPipelineStateWithDescriptor:pipelineStateDesc error:nil]);
 
         pipelineStateDesc.sampleCount = 1;
 
-        _pipeline_state_without_texture.move_object(
+        this->_pipeline_state_without_texture.move_object(
             [device newRenderPipelineStateWithDescriptor:pipelineStateDesc error:nil]);
 
         pipelineStateDesc.fragmentFunction = fragmentProgramWithTexture;
 
-        _pipeline_state_with_texture.move_object(
+        this->_pipeline_state_with_texture.move_object(
             [device newRenderPipelineStateWithDescriptor:pipelineStateDesc error:nil]);
     }
 
     void prepare_uniforms_buffer(uint32_t const uniforms_count) override {
         bool needs_allocate = false;
         NSUInteger length = uniforms_count * sizeof(uniforms2d_t);
-        length = length - length % _uniforms_buffer_allocating_unit + _uniforms_buffer_allocating_unit;
+        length = length - length % ui::_uniforms_buffer_allocating_unit + ui::_uniforms_buffer_allocating_unit;
 
-        if (auto &current_buffer = _uniforms_buffers[_uniforms_buffer_index]) {
+        if (auto &current_buffer = this->_uniforms_buffers[this->_uniforms_buffer_index]) {
             id<MTLBuffer> currentBuffer = current_buffer.object();
             auto const prev_length = currentBuffer.length;
             if (prev_length < length) {
@@ -107,8 +107,8 @@ struct ui::metal_system::impl : base::impl,
         }
 
         if (needs_allocate) {
-            _uniforms_buffers[_uniforms_buffer_index].move_object(
-                [_device.object() newBufferWithLength:length options:kNilOptions]);
+            this->_uniforms_buffers[this->_uniforms_buffer_index].move_object(
+                [this->_device.object() newBufferWithLength:length options:kNilOptions]);
         }
     }
 
@@ -118,8 +118,8 @@ struct ui::metal_system::impl : base::impl,
         }
 
         auto view = (YASUIMetalView * const)objc_view;
-        view.device = _device.object();
-        view.sampleCount = _sample_count;
+        view.device = this->_device.object();
+        view.sampleCount = this->_sample_count;
     }
 
     void view_render(yas_objc_view *const objc_view, ui::renderer &renderer) override {
@@ -136,22 +136,22 @@ struct ui::metal_system::impl : base::impl,
             return;
         }
 
-        dispatch_semaphore_wait(_inflight_semaphore.object(), DISPATCH_TIME_FOREVER);
+        dispatch_semaphore_wait(this->_inflight_semaphore.object(), DISPATCH_TIME_FOREVER);
 
-        auto command_buffer = make_objc_ptr<id<MTLCommandBuffer>>([commandQueue = _command_queue.object()]() {
+        auto command_buffer = make_objc_ptr<id<MTLCommandBuffer>>([commandQueue = this->_command_queue.object()]() {
             return [commandQueue commandBuffer];
         });
         auto commandBuffer = command_buffer.object();
 
-        _uniforms_buffer_offset = 0;
+        this->_uniforms_buffer_offset = 0;
 
-        _render_nodes(renderer, commandBuffer, renderPassDesc);
+        this->_render_nodes(renderer, commandBuffer, renderPassDesc);
 
-        [commandBuffer addCompletedHandler:[semaphore = _inflight_semaphore](id<MTLCommandBuffer> _Nonnull) {
+        [commandBuffer addCompletedHandler:[semaphore = this->_inflight_semaphore](id<MTLCommandBuffer> _Nonnull) {
             dispatch_semaphore_signal(semaphore.object());
         }];
 
-        _uniforms_buffer_index = (_uniforms_buffer_index + 1) % _uniforms_buffer_count;
+        this->_uniforms_buffer_index = (this->_uniforms_buffer_index + 1) % ui::_uniforms_buffer_count;
 
         [commandBuffer presentDrawable:currentDrawable];
         [commandBuffer commit];
@@ -159,17 +159,17 @@ struct ui::metal_system::impl : base::impl,
 
     void mesh_encode(ui::mesh &mesh, id<MTLRenderCommandEncoder> const encoder,
                      ui::metal_encode_info const &encode_info) override {
-        auto const currentUniformsBuffer = _uniforms_buffers[_uniforms_buffer_index].object();
+        auto const currentUniformsBuffer = this->_uniforms_buffers[this->_uniforms_buffer_index].object();
 
         if (auto uniforms_ptr =
-                (uniforms2d_t *)(&((uint8_t *)[currentUniformsBuffer contents])[_uniforms_buffer_offset])) {
+                (uniforms2d_t *)(&((uint8_t *)[currentUniformsBuffer contents])[this->_uniforms_buffer_offset])) {
             uniforms_ptr->matrix = mesh.renderable().matrix();
             uniforms_ptr->color = mesh.color();
             uniforms_ptr->use_mesh_color = mesh.is_use_mesh_color();
         }
 
         if (auto &texture = mesh.texture()) {
-            [encoder setFragmentBuffer:currentUniformsBuffer offset:_uniforms_buffer_offset atIndex:0];
+            [encoder setFragmentBuffer:currentUniformsBuffer offset:this->_uniforms_buffer_offset atIndex:0];
             [encoder setRenderPipelineState:encode_info.pipelineStateWithTexture()];
             [encoder setFragmentTexture:texture.metal_texture().texture() atIndex:0];
             [encoder setFragmentSamplerState:texture.metal_texture().samplerState() atIndex:0];
@@ -183,7 +183,7 @@ struct ui::metal_system::impl : base::impl,
         [encoder setVertexBuffer:renderable_mesh_data.vertexBuffer()
                           offset:renderable_mesh_data.vertex_buffer_byte_offset()
                          atIndex:0];
-        [encoder setVertexBuffer:currentUniformsBuffer offset:_uniforms_buffer_offset atIndex:1];
+        [encoder setVertexBuffer:currentUniformsBuffer offset:this->_uniforms_buffer_offset atIndex:1];
 
         [encoder drawIndexedPrimitives:to_mtl_primitive_type(mesh.primitive_type())
                             indexCount:mesh_data.index_count()
@@ -191,24 +191,24 @@ struct ui::metal_system::impl : base::impl,
                            indexBuffer:renderable_mesh_data.indexBuffer()
                      indexBufferOffset:renderable_mesh_data.index_buffer_byte_offset()];
 
-        _uniforms_buffer_offset += sizeof(uniforms2d_t);
-        assert(_uniforms_buffer_offset + sizeof(uniforms2d_t) < currentUniformsBuffer.length);
+        this->_uniforms_buffer_offset += sizeof(uniforms2d_t);
+        assert(this->_uniforms_buffer_offset + sizeof(uniforms2d_t) < currentUniformsBuffer.length);
     }
 
     id<MTLDevice> mtlDevice() override {
-        return _device.object();
+        return this->_device.object();
     }
 
     uint32_t sample_count() override {
-        return _sample_count;
+        return this->_sample_count;
     }
 
     id<MTLRenderPipelineState> mtlRenderPipelineStateWithTexture() override {
-        return _pipeline_state_with_texture.object();
+        return this->_pipeline_state_with_texture.object();
     }
 
     id<MTLRenderPipelineState> mtlRenderPipelineStateWithoutTexture() override {
-        return _pipeline_state_without_texture.object();
+        return this->_pipeline_state_without_texture.object();
     }
 
     objc_ptr<id<MTLTexture>> make_mtl_texture(MTLTextureDescriptor *const textureDesc) override {
@@ -249,8 +249,8 @@ struct ui::metal_system::impl : base::impl,
         ui::metal_render_encoder metal_render_encoder;
         metal_render_encoder.push_encode_info(
             {{.renderPassDescriptor = renderPassDesc,
-              .pipelineStateWithTexture = _multi_sample_pipeline_state_with_texture.object(),
-              .pipelineStateWithoutTexture = _multi_sample_pipeline_state_without_texture.object()}});
+              .pipelineStateWithTexture = this->_multi_sample_pipeline_state_with_texture.object(),
+              .pipelineStateWithoutTexture = this->_multi_sample_pipeline_state_without_texture.object()}});
 
         ui::render_info render_info{.detector = renderer.detector(),
                                     .render_encodable = metal_render_encoder.encodable(),
@@ -281,19 +281,19 @@ ui::metal_system::metal_system(std::nullptr_t) : base(nullptr) {
 ui::metal_system::~metal_system() = default;
 
 ui::makable_metal_system &ui::metal_system::makable() {
-    if (!_makable) {
-        _makable = ui::makable_metal_system{impl_ptr<ui::makable_metal_system::impl>()};
+    if (!this->_makable) {
+        this->_makable = ui::makable_metal_system{impl_ptr<ui::makable_metal_system::impl>()};
     }
 
-    return _makable;
+    return this->_makable;
 }
 
 ui::renderable_metal_system &ui::metal_system::renderable() {
-    if (!_renderable) {
-        _renderable = ui::renderable_metal_system{impl_ptr<ui::renderable_metal_system::impl>()};
+    if (!this->_renderable) {
+        this->_renderable = ui::renderable_metal_system{impl_ptr<ui::renderable_metal_system::impl>()};
     }
 
-    return _renderable;
+    return this->_renderable;
 }
 
 #if YAS_TEST
