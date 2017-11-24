d34aa6ecbc22b85e9fc0130641e62e8caf3c4ad9
Yuki Yasoshima <yukiyasos@gmail.com>
2017-11-24 19:10:20 +0900

    event this

--- ui/yas_ui_event.cpp
+++ ui/yas_ui_event.cpp
@@ -37,9 +37,9 @@ struct ui::event::impl : impl_base {
 
     virtual bool is_equal(std::shared_ptr<base::impl> const &rhs) const override {
         if (auto casted_rhs = std::dynamic_pointer_cast<impl>(rhs)) {
-            auto &type_info = type();
+            auto &type_info = this->type();
             if (type_info == casted_rhs->type()) {
-                return value == casted_rhs->value;
+                return this->value == casted_rhs->value;
             }
         }
 
@@ -94,10 +94,10 @@ template ui::key::type const &ui::event::get<ui::key>() const;
 template ui::modifier::type const &ui::event::get<ui::modifier>() const;
 
 ui::manageable_event &ui::event::manageable() {
-    if (!_manageable) {
-        _manageable = ui::manageable_event{impl_ptr<ui::manageable_event::impl>()};
+    if (!this->_manageable) {
+        this->_manageable = ui::manageable_event{impl_ptr<ui::manageable_event::impl>()};
     }
-    return _manageable;
+    return this->_manageable;
 }
 
 #pragma mark - event_manager::impl
@@ -107,25 +107,25 @@ struct ui::event_manager::impl : base::impl, event_inputtable::impl {
         ui::event_phase phase;
 
         if (value.contains_in_window()) {
-            if (cursor_event) {
+            if (this->_cursor_event) {
                 phase = event_phase::changed;
             } else {
                 phase = event_phase::began;
-                cursor_event = ui::event{cursor_tag};
+                this->_cursor_event = ui::event{cursor_tag};
             }
         } else {
             phase = event_phase::ended;
         }
 
-        if (cursor_event) {
-            auto manageable_event = cursor_event.manageable();
+        if (this->_cursor_event) {
+            auto manageable_event = this->_cursor_event.manageable();
             manageable_event.set_phase(phase);
             manageable_event.set<cursor>(std::move(value));
 
-            subject.notify(event_manager::method::cursor_changed, cursor_event);
+            this->_subject.notify(event_manager::method::cursor_changed, this->_cursor_event);
 
             if (phase == event_phase::ended) {
-                cursor_event = nullptr;
+                this->_cursor_event = nullptr;
             }
         }
     }
@@ -134,23 +134,23 @@ struct ui::event_manager::impl : base::impl, event_inputtable::impl {
         auto const identifer = value.identifier();
 
         if (phase == event_phase::began) {
-            if (touch_events.count(identifer) > 0) {
+            if (this->_touch_events.count(identifer) > 0) {
                 return;
             }
             ui::event event{touch_tag};
-            touch_events.emplace(std::make_pair(identifer, std::move(event)));
+            this->_touch_events.emplace(std::make_pair(identifer, std::move(event)));
         }
 
-        if (touch_events.count(identifer) > 0) {
-            auto &event = touch_events.at(identifer);
+        if (this->_touch_events.count(identifer) > 0) {
+            auto &event = this->_touch_events.at(identifer);
             auto manageable_event = event.manageable();
             manageable_event.set_phase(phase);
             manageable_event.set<touch>(std::move(value));
 
-            subject.notify(event_manager::method::touch_changed, event);
+            this->_subject.notify(event_manager::method::touch_changed, event);
 
             if (phase == event_phase::ended || phase == event_phase::canceled) {
-                touch_events.erase(identifer);
+                this->_touch_events.erase(identifer);
             }
         }
     }
@@ -159,22 +159,22 @@ struct ui::event_manager::impl : base::impl, event_inputtable::impl {
         auto const key_code = value.key_code();
 
         if (phase == event_phase::began) {
-            if (key_events.count(key_code) > 0) {
+            if (this->_key_events.count(key_code) > 0) {
                 return;
             }
             ui::event event{key_tag};
-            key_events.emplace(std::make_pair(key_code, std::move(event)));
+            this->_key_events.emplace(std::make_pair(key_code, std::move(event)));
         }
 
-        if (key_events.count(key_code) > 0) {
-            auto &event = key_events.at(key_code);
+        if (this->_key_events.count(key_code) > 0) {
+            auto &event = this->_key_events.at(key_code);
             event.manageable().set_phase(phase);
             event.manageable().set<key>(value);
 
-            subject.notify(event_manager::method::key_changed, event);
+            this->_subject.notify(event_manager::method::key_changed, event);
 
             if (phase == event_phase::ended || phase == event_phase::canceled) {
-                key_events.erase(key_code);
+                this->_key_events.erase(key_code);
             }
         }
     }
@@ -186,32 +186,32 @@ struct ui::event_manager::impl : base::impl, event_inputtable::impl {
 
         for (auto const &flag : all_flags) {
             if (flags & flag) {
-                if (modifier_events.count(flag) == 0) {
+                if (this->_modifier_events.count(flag) == 0) {
                     ui::event event{modifier_tag};
                     event.manageable().set<modifier>(ui::modifier_event{flag, timestamp});
                     event.manageable().set_phase(ui::event_phase::began);
-                    modifier_events.emplace(std::make_pair(flag, std::move(event)));
+                    this->_modifier_events.emplace(std::make_pair(flag, std::move(event)));
 
-                    subject.notify(event_manager::method::modifier_changed, modifier_events.at(flag));
+                    this->_subject.notify(event_manager::method::modifier_changed, this->_modifier_events.at(flag));
                 }
             } else {
-                if (modifier_events.count(flag) > 0) {
-                    auto &event = modifier_events.at(flag);
+                if (this->_modifier_events.count(flag) > 0) {
+                    auto &event = this->_modifier_events.at(flag);
                     event.manageable().set_phase(ui::event_phase::ended);
 
-                    subject.notify(event_manager::method::modifier_changed, event);
+                    this->_subject.notify(event_manager::method::modifier_changed, event);
 
-                    modifier_events.erase(flag);
+                    this->_modifier_events.erase(flag);
                 }
             }
         }
     }
 
-    event cursor_event{nullptr};
-    std::unordered_map<uintptr_t, event> touch_events;
-    std::unordered_map<uint16_t, event> key_events;
-    std::unordered_map<uint32_t, event> modifier_events;
-    yas::subject<ui::event, ui::event_manager::method> subject;
+    event _cursor_event{nullptr};
+    std::unordered_map<uintptr_t, event> _touch_events;
+    std::unordered_map<uint16_t, event> _key_events;
+    std::unordered_map<uint32_t, event> _modifier_events;
+    yas::subject<ui::event, ui::event_manager::method> _subject;
 };
 
 #pragma mark - manageable_event
@@ -251,7 +251,7 @@ ui::event_manager::event_manager(std::nullptr_t) : base(nullptr) {
 ui::event_manager::~event_manager() = default;
 
 subject<ui::event, ui::event_manager::method> &ui::event_manager::subject() {
-    return impl_ptr<impl>()->subject;
+    return impl_ptr<impl>()->_subject;
 }
 
 ui::event_inputtable &ui::event_manager::inputtable() {
