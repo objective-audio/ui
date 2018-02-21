826b976d3aa34a414803b8f1678e590e03aaefdf
Yuki Yasoshima <yukiyasos@gmail.com>
2018-02-22 00:15:06 +0900

    update sample

--- ui_sample_common/yas_sample_main.mm
+++ ui_sample_common/yas_sample_main.mm
@@ -54,35 +54,12 @@ void sample::main::setup() {
     button_pos_action.set_value_transformer([](float const value) { return sinf(M_PI * 2.0f * value); });
     this->renderer.insert_action(std::move(button_pos_action));
 
-    auto update_texture_handler = [
-        weak_font_atlas = to_weak(_font_atlas), weak_big_button = to_weak(_big_button),
-        weak_touch_holder = to_weak(_touch_holder)
-    ](ui::renderer const &renderer) {
-        auto const scale_factor = renderer.scale_factor();
+    ui::texture texture{{.point_size = {1024, 1024}}};
+    texture.observe_scale_from_renderer(this->renderer);
 
-        ui::texture texture = nullptr;
-        if (scale_factor > 0) {
-            texture = ui::texture{{.point_size = {1024, 1024}, .scale_factor = scale_factor}};
-        }
-
-        if (auto font_atlas = weak_font_atlas.lock()) {
-            font_atlas.set_texture(texture);
-        }
-
-        if (auto big_button = weak_big_button.lock()) {
-            big_button.set_texture(texture);
-        }
-
-        if (auto touch_holder = weak_touch_holder.lock()) {
-            touch_holder.set_texture(texture);
-        }
-    };
-
-    this->_scale_observer = this->renderer.subject().make_observer(
-        ui::renderer::method::scale_factor_changed,
-        [update_texture_handler](auto const &context) mutable { update_texture_handler(context.value); });
-
-    update_texture_handler(this->renderer);
+    this->_font_atlas.set_texture(texture);
+    this->_big_button.set_texture(texture);
+    this->_touch_holder.set_texture(texture);
 
     ui::render_target render_target;
     render_target.observe_scale_from_renderer(this->renderer);
