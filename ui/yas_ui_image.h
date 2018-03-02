6b69800966a5c297b4cdbc2b589909d9093fe144
Yuki Yasoshima <yukiyasos@gmail.com>
2018-03-02 13:27:35 +0900

    draw_handler

--- ui/yas_ui_image.h
+++ ui/yas_ui_image.h
@@ -20,6 +20,8 @@ class image : public base {
         ui::uint_size point_size;
         double scale_factor = 1.0;
     };
+    
+    using draw_handler_f = std::function<void(CGContextRef const)>;
 
     image(args);
     image(std::nullptr_t);
@@ -34,6 +36,6 @@ class image : public base {
     void *data();
 
     void clear();
-    void draw(std::function<void(CGContextRef const)> const &);
+    void draw(draw_handler_f const &);
 };
 }
