869cb234080f198fe5c3615250ad870b0f3f0bfa
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-13 23:55:53 +0900

    fix collection_layout

--- ui/yas_ui_collection_layout.cpp
+++ ui/yas_ui_collection_layout.cpp
@@ -563,19 +563,19 @@ flow::node<std::size_t> ui::collection_layout::begin_actual_cell_count_flow() co
     return impl_ptr<impl>()->_actual_cell_count_property.begin_value_flow();
 }
 
-[[nodiscard]] flow::node<ui::size> ui::collection_layout::begin_default_cell_size_flow() const {
+flow::node<ui::size> ui::collection_layout::begin_default_cell_size_flow() const {
     return impl_ptr<impl>()->_default_cell_size_property.begin_value_flow();
 }
 
-    [[nodiscard]] flow::node<std::vector<ui::collection_layout::line>> ui::collection_layout::begin_lines_flow() const {
+flow::node<std::vector<ui::collection_layout::line>> ui::collection_layout::begin_lines_flow() const {
     return impl_ptr<impl>()->_lines_property.begin_value_flow();
 }
 
-[[nodiscard]] flow::node<float> ui::collection_layout::begin_row_spacing_flow() const {
+flow::node<float> ui::collection_layout::begin_row_spacing_flow() const {
     return impl_ptr<impl>()->_row_spacing_property.begin_value_flow();
 }
 
-    [[nodiscard]] flow::node<float> ui::collection_layout::begin_col_spacing_flow() const {
+flow::node<float> ui::collection_layout::begin_col_spacing_flow() const {
     return impl_ptr<impl>()->_col_spacing_property.begin_value_flow();
 }
 
@@ -583,14 +583,14 @@ flow::node<ui::layout_alignment> ui::collection_layout::begin_alignment_flow() c
     return impl_ptr<impl>()->_alignment_property.begin_value_flow();
 }
 
-[[nodiscard]] flow::node<ui::layout_direction> ui::collection_layout::begin_direction_flow() const {
+flow::node<ui::layout_direction> ui::collection_layout::begin_direction_flow() const {
     return impl_ptr<impl>()->_direction_property.begin_value_flow();
 }
 
-    [[nodiscard]] flow::node<ui::layout_order> ui::collection_layout::begin_row_order_flow() const {
+flow::node<ui::layout_order> ui::collection_layout::begin_row_order_flow() const {
     return impl_ptr<impl>()->_row_order_property.begin_value_flow();
 }
 
-[[nodiscard]] flow::node<ui::layout_order> ui::collection_layout::begin_col_order_flow() const {
+flow::node<ui::layout_order> ui::collection_layout::begin_col_order_flow() const {
     return impl_ptr<impl>()->_col_order_property.begin_value_flow();
 }
