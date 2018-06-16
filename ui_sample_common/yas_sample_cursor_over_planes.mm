ba6a7453d9b32859abf8d912500aec317c2897fe
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-16 21:32:02 +0900

    sample cursor_over_planes

--- ui_sample_common/yas_sample_cursor_over_planes.mm
+++ ui_sample_common/yas_sample_cursor_over_planes.mm
@@ -16,19 +16,20 @@ struct sample::cursor_over_planes::impl : base::impl {
     }
 
     void prepare(sample::cursor_over_planes &planes) {
-        this->_renderer_observer = root_node.dispatch_and_make_observer(
-            ui::node::method::renderer_changed,
-            [weak_touch_holder = to_weak(planes), event_observers = std::vector<base>{}](auto const &context) mutable {
-                if (auto touch_holder = weak_touch_holder.lock()) {
-                    auto impl = touch_holder.impl_ptr<cursor_over_planes::impl>();
-                    auto &node = context.value;
-                    if (auto renderer = node.renderer()) {
-                        event_observers = _make_event_observers(impl->_nodes, renderer);
-                    } else {
-                        event_observers.clear();
+        this->_renderer_flow =
+            root_node.begin_renderer_flow()
+                .perform([weak_touch_holder = to_weak(planes),
+                          event_observers = std::vector<flow::observer>{}](ui::renderer const &value) mutable {
+                    if (auto touch_holder = weak_touch_holder.lock()) {
+                        auto impl = touch_holder.impl_ptr<cursor_over_planes::impl>();
+                        if (auto renderer = value) {
+                            event_observers = _make_event_observers(impl->_nodes, renderer);
+                        } else {
+                            event_observers.clear();
+                        }
                     }
-                }
-            });
+                })
+                .end();
     }
 
    private:
@@ -58,45 +59,48 @@ struct sample::cursor_over_planes::impl : base::impl {
         }
     }
 
-    static std::vector<base> _make_event_observers(std::vector<ui::node> const &nodes, ui::renderer &renderer) {
-        std::vector<base> event_observers;
+    static std::vector<flow::observer> _make_event_observers(std::vector<ui::node> const &nodes,
+                                                             ui::renderer &renderer) {
+        std::vector<flow::observer> event_observers;
         event_observers.reserve(nodes.size());
 
         for (auto &node : nodes) {
-            event_observers.emplace_back(renderer.event_manager().subject().make_observer(
-                ui::event_manager::method::cursor_changed,
-                [weak_node = to_weak(node), prev_detected = std::make_shared<bool>(false)](auto const &context) {
-                    ui::event const &event = context.value;
-                    auto cursor_event = event.get<ui::cursor>();
+            event_observers.emplace_back(
+                renderer.event_manager()
+                    .begin_flow(ui::event_manager::method::cursor_changed)
+                    .perform([weak_node = to_weak(node),
+                              prev_detected = std::make_shared<bool>(false)](ui::event const &event) {
+                        auto cursor_event = event.get<ui::cursor>();
 
-                    if (auto node = weak_node.lock()) {
-                        if (auto renderer = node.renderer()) {
-                            auto is_detected = renderer.detector().detect(cursor_event.position(), node.collider());
+                        if (auto node = weak_node.lock()) {
+                            if (auto renderer = node.renderer()) {
+                                auto is_detected = renderer.detector().detect(cursor_event.position(), node.collider());
 
-                            auto make_color_action = [](ui::node &node, ui::color const &color) {
-                                return ui::make_action(
-                                    {.target = node, .begin_color = node.color(), .end_color = color});
-                            };
+                                auto make_color_action = [](ui::node &node, ui::color const &color) {
+                                    return ui::make_action(
+                                        {.target = node, .begin_color = node.color(), .end_color = color});
+                                };
 
-                            if (is_detected && !*prev_detected) {
-                                renderer.erase_action(node);
-                                renderer.insert_action(make_color_action(node, {1.0f, 0.6f, 0.0f}));
-                            } else if (!is_detected && *prev_detected) {
-                                renderer.erase_action(node);
-                                renderer.insert_action(make_color_action(node, {0.3f, 0.3f, 0.3f}));
+                                if (is_detected && !*prev_detected) {
+                                    renderer.erase_action(node);
+                                    renderer.insert_action(make_color_action(node, {1.0f, 0.6f, 0.0f}));
+                                } else if (!is_detected && *prev_detected) {
+                                    renderer.erase_action(node);
+                                    renderer.insert_action(make_color_action(node, {0.3f, 0.3f, 0.3f}));
+                                }
+
+                                *prev_detected = is_detected;
                             }
-
-                            *prev_detected = is_detected;
                         }
-                    }
-                }));
+                    })
+                    .end());
         }
 
         return event_observers;
     }
 
     std::vector<ui::node> _nodes;
-    ui::node::observer_t _renderer_observer = nullptr;
+    flow::observer _renderer_flow = nullptr;
 };
 
 sample::cursor_over_planes::cursor_over_planes() : base(std::make_shared<impl>()) {
