ef355888c20f4f5fa7bab30f8536143ad71ba6cd
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-10 10:06:32 +0900

    add ui_mac_test_app

---
+++ ui_mac_tests/ui_map_test_app/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
