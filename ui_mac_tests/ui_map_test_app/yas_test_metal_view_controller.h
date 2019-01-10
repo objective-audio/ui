5ac77b3ccd7ab4f7fea570a8edef1fb43117d268
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-10 10:11:34 +0900

    fix ui_mac_test_app

---
+++ ui_mac_tests/ui_map_test_app/yas_test_metal_view_controller.h
@@ -0,0 +1,11 @@
+//
+//  yas_test_metal_view_controller.h
+//
+
+#import <ui/yas_ui_metal_view_controller.h>
+
+@interface YASTestMetalViewController : YASUIMetalViewController
+
++ (instancetype)sharedViewController;
+
+@end
