72bd35a02c3515e60f7b3dab776bd9eb850ae858
Yuki Yasoshima <yukiyasos@gmail.com>
2019-08-03 11:57:47 +0900

    blur remove impl

--- ui/yas_ui_blur.h
+++ ui/yas_ui_blur.h
@@ -4,21 +4,26 @@
 
 #pragma once
 
-#include <cpp_utils/yas_base.h>
+#include <chaining/yas_chaining_umbrella.h>
 
 namespace yas::ui {
 class effect;
 
-struct blur : base {
-    class impl;
-
+struct blur {
     void set_sigma(double const);
     double sigma() const;
 
     ui::effect &effect();
 
    private:
+    chaining::value::holder<double> _sigma{0.0};
+    std::unique_ptr<ui::effect> _effect;
+    chaining::any_observer_ptr _sigma_observer = nullptr;
+
     blur();
+
+    void prepare(std::shared_ptr<blur> &);
+    void _update_effect_handler();
 
    public:
     static std::shared_ptr<blur> make_shared();
--- ui/yas_ui_blur.mm
+++ ui/yas_ui_blur.mm
@@ -4,74 +4,64 @@
 
 #include "yas_ui_blur.h"
 #include <MetalPerformanceShaders/MetalPerformanceShaders.h>
-#include <chaining/yas_chaining_umbrella.h>
 #include "yas_ui_effect.h"
 #include "yas_ui_metal_texture.h"
 #include "yas_ui_texture.h"
 
 using namespace yas;
 
-struct ui::blur::impl : base::impl {
-    chaining::value::holder<double> _sigma{0.0};
-    ui::effect _effect;
-
-    void prepare(std::shared_ptr<ui::blur> &blur) {
-        auto weak_blur = to_weak(blur);
-
-        this->_sigma_observer =
-            this->_sigma.chain()
-                .guard([weak_blur](double const &) { return !weak_blur.expired(); })
-                .perform([weak_blur](double const &) { weak_blur.lock()->impl_ptr<impl>()->_update_effect_handler(); })
-                .sync();
-    }
-
-   private:
-    chaining::any_observer_ptr _sigma_observer = nullptr;
-
-    void _update_effect_handler() {
-        double const sigma = this->_sigma.raw();
-
-        if (sigma > 0.0) {
-            this->_effect.set_metal_handler(
-                [sigma, prev_scale_factor = std::optional<double>{std::nullopt},
-                 blur = objc_ptr<MPSImageGaussianBlur *>{nullptr}](ui::texture &src_texture, ui::texture &dst_texture,
-                                                                   ui::metal_system &metal_system,
-                                                                   id<MTLCommandBuffer> const commandBuffer) mutable {
-                    double const scale_factor = src_texture.scale_factor();
-
-                    if (!prev_scale_factor || scale_factor != *prev_scale_factor) {
-                        prev_scale_factor = scale_factor;
-                        double const blur_sigma = sigma * scale_factor;
-                        blur = metal_system.makable().make_mtl_blur(blur_sigma);
-                    }
-
-                    auto const srcTexture = src_texture.metal_texture().texture();
-                    auto const dstTexture = dst_texture.metal_texture().texture();
-                    [*blur encodeToCommandBuffer:commandBuffer sourceTexture:srcTexture destinationTexture:dstTexture];
-                });
-        } else {
-            this->_effect.set_metal_handler(ui::effect::through_metal_handler());
-        }
-    }
-};
-
-ui::blur::blur() : base(std::make_shared<blur::impl>()) {
+ui::blur::blur() : _effect(std::make_unique<ui::effect>()) {
 }
 
 void ui::blur::set_sigma(double const sigma) {
-    impl_ptr<impl>()->_sigma.set_value(sigma);
+    this->_sigma.set_value(sigma);
 }
 
 double ui::blur::sigma() const {
-    return impl_ptr<impl>()->_sigma.raw();
+    return this->_sigma.raw();
 }
 
 ui::effect &ui::blur::effect() {
-    return impl_ptr<impl>()->_effect;
+    return *this->_effect;
+}
+
+void ui::blur::prepare(std::shared_ptr<ui::blur> &blur) {
+    auto weak_blur = to_weak(blur);
+
+    this->_sigma_observer = this->_sigma.chain()
+                                .guard([weak_blur](double const &) { return !weak_blur.expired(); })
+                                .perform([weak_blur](double const &) { weak_blur.lock()->_update_effect_handler(); })
+                                .sync();
+}
+
+void ui::blur::_update_effect_handler() {
+    double const sigma = this->_sigma.raw();
+
+    if (sigma > 0.0) {
+        this->_effect->set_metal_handler(
+            [sigma, prev_scale_factor = std::optional<double>{std::nullopt},
+             blur = objc_ptr<MPSImageGaussianBlur *>{nullptr}](ui::texture &src_texture, ui::texture &dst_texture,
+                                                               ui::metal_system &metal_system,
+                                                               id<MTLCommandBuffer> const commandBuffer) mutable {
+                double const scale_factor = src_texture.scale_factor();
+
+                if (!prev_scale_factor || scale_factor != *prev_scale_factor) {
+                    prev_scale_factor = scale_factor;
+                    double const blur_sigma = sigma * scale_factor;
+                    blur = metal_system.makable().make_mtl_blur(blur_sigma);
+                }
+
+                auto const srcTexture = src_texture.metal_texture().texture();
+                auto const dstTexture = dst_texture.metal_texture().texture();
+                [*blur encodeToCommandBuffer:commandBuffer sourceTexture:srcTexture destinationTexture:dstTexture];
+            });
+    } else {
+        this->_effect->set_metal_handler(ui::effect::through_metal_handler());
+    }
 }
 
 std::shared_ptr<ui::blur> ui::blur::make_shared() {
     auto shared = std::shared_ptr<blur>(new blur{});
-    shared->impl_ptr<impl>()->prepare(shared);
+    shared->prepare(shared);
     return shared;
 }
