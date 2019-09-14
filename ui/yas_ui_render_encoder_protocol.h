731996ebe0f3a30827c885acfe537de37e9d8d1b
Yuki Yasoshima <yukiyasos@gmail.com>
2019-09-14 23:50:37 +0900

    render_encoder

--- ui/yas_ui_render_encoder_protocol.h
+++ ui/yas_ui_render_encoder_protocol.h
@@ -9,10 +9,21 @@
 #include "yas_ui_metal_encode_info.h"
 
 namespace yas::ui {
+class render_encodable;
+class render_effectable;
+class render_stackable;
+using render_encodable_ptr = std::shared_ptr<render_encodable>;
+using render_effectable_ptr = std::shared_ptr<render_effectable>;
+using render_stackable_ptr = std::shared_ptr<render_stackable>;
+
 struct render_encodable {
     virtual ~render_encodable() = default;
 
     virtual void append_mesh(ui::mesh_ptr const &mesh) = 0;
+
+    static render_encodable_ptr cast(render_encodable_ptr const &encodable) {
+        return encodable;
+    }
 };
 
 using render_encodable_ptr = std::shared_ptr<render_encodable>;
@@ -21,6 +32,10 @@ struct render_effectable {
     virtual ~render_effectable() = default;
 
     virtual void append_effect(ui::effect_ptr const &effect) = 0;
+
+    static render_effectable_ptr cast(render_effectable_ptr const &effectable) {
+        return effectable;
+    }
 };
 
 using render_effectable_ptr = std::shared_ptr<render_effectable>;
@@ -31,6 +46,10 @@ struct render_stackable {
     virtual void push_encode_info(ui::metal_encode_info_ptr const &) = 0;
     virtual void pop_encode_info() = 0;
     virtual ui::metal_encode_info_ptr const &current_encode_info() = 0;
+
+    static render_stackable_ptr cast(render_stackable_ptr const &stackable) {
+        return stackable;
+    }
 };
 
 using render_stackable_ptr = std::shared_ptr<render_stackable>;
