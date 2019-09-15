078d55ed24cff0b6450706bd1869f04b96bb381e
Yuki Yasoshima <yukiyasos@gmail.com>
2019-09-15 16:53:33 +0900

    fix sample

--- ui_sample_common/YASSampleMetalViewController.mm
+++ ui_sample_common/YASSampleMetalViewController.mm
@@ -17,7 +17,7 @@ using namespace yas;
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self setRenderable:_main.renderer->view_renderable()];
+    [self setRenderable:_main.renderer];
 
     self->_main.setup();
 
