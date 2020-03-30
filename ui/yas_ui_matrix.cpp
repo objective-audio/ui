7e97b0703066cae5b34eaf68874498777cd7adee
Yuki Yasoshima <yukiyasos@gmail.com>
2020-03-30 23:29:24 +0900

    const

--- ui/yas_ui_matrix.cpp
+++ ui/yas_ui_matrix.cpp
@@ -18,9 +18,9 @@ float4x4 ui::matrix::translation(float const x, float const y) {
 }
 
 float4x4 ui::matrix::rotation(float const degree) {
-    float radians = degree * M_PI / 180.0f;
-    float cos = cosf(radians);
-    float sin = sinf(radians);
+    float const radians = degree * M_PI / 180.0f;
+    float const cos = cosf(radians);
+    float const sin = sinf(radians);
 
     return float4x4{float4{cos, sin, 0.0f, 0.0f}, float4{-sin, cos, 0.0f, 0.0f}, float4{0.0f, 0.0f, 1.0f, 0.0f},
                     float4{0.0f, 0.0f, 0.0f, 1.0f}};
@@ -28,12 +28,12 @@ float4x4 ui::matrix::rotation(float const degree) {
 
 float4x4 ui::matrix::ortho(float const left, float const right, float const bottom, float const top, float const near,
                            float const far) {
-    float ral = right + left;
-    float rsl = right - left;
-    float tab = top + bottom;
-    float tsb = top - bottom;
-    float fan = far + near;
-    float fsn = far - near;
+    float const ral = right + left;
+    float const rsl = right - left;
+    float const tab = top + bottom;
+    float const tsb = top - bottom;
+    float const fan = far + near;
+    float const fsn = far - near;
 
     return float4x4{float4{2.0f / rsl, 0.0f, 0.0f, 0.0f}, float4{0.0f, 2.0f / tsb, 0.0f, 0.0f},
                     float4{0.0f, 0.0f, -2.0f / fsn, 0.0f}, float4{-ral / rsl, -tab / tsb, -fan / fsn, 1.0f}};
