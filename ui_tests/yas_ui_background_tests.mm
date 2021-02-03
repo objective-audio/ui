d69825889a98bc2ed366fbff3a8cf7cedc8cf826
Yuki Yasoshima <yukiyasos@gmail.com>
2021-02-03 22:46:15 +0900

    add tests
    add tests

--- ui_tests/yas_ui_background_tests.mm
+++ ui_tests/yas_ui_background_tests.mm
@@ -36,4 +36,42 @@ using namespace yas;
     XCTAssertTrue(renderable->updates().flags.any());
 }
 
+- (void)test_color {
+    auto const background = ui::background::make_shared();
+
+    XCTAssertTrue(background->color() == (ui::color{.v = 1.0f}));
+
+    std::vector<ui::color> called;
+
+    auto canceller = background->observe_color([&called](ui::color const &color) { called.emplace_back(color); }, true);
+
+    XCTAssertEqual(called.size(), 1);
+    XCTAssertTrue(called.at(0) == (ui::color{.v = 1.0f}));
+
+    background->set_color({.red = 1.0f, .green = 0.5f, .blue = 0.25f});
+
+    XCTAssertTrue(background->color() == (ui::color{.red = 1.0f, .green = 0.5f, .blue = 0.25f}));
+    XCTAssertEqual(called.size(), 2);
+    XCTAssertTrue(called.at(1) == (ui::color{.red = 1.0f, .green = 0.5f, .blue = 0.25f}));
+}
+
+- (void)test_alpha {
+    auto const background = ui::background::make_shared();
+
+    XCTAssertEqual(background->alpha(), 1.0f);
+
+    std::vector<float> called;
+
+    auto canceller = background->observe_alpha([&called](float const &alpha) { called.emplace_back(alpha); }, true);
+
+    XCTAssertEqual(called.size(), 1);
+    XCTAssertEqual(called.at(0), 1.0f);
+
+    background->set_alpha(0.5f);
+
+    XCTAssertEqual(background->alpha(), 0.5f);
+    XCTAssertEqual(called.size(), 2);
+    XCTAssertEqual(called.at(1), 0.5f);
+}
+
 @end
