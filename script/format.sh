12ba640e86a20a2bb2acfa64bc1f883b588d2cfb
Yuki Yasoshima <yukiyasos@gmail.com>
2021-02-07 10:21:16 +0900

    format

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+export PATH=$PATH:/opt/homebrew/bin
 clang-format -i -style=file `find ../ui ../ui_tests ../ui_sample_common ../ui_mac_sample ../ui_ios_sample ../ui_mac_tests -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
