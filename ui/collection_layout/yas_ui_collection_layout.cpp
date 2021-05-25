66a2b29b2075a85ff333a95f803aa3a47482f8fd
Yuki Yasoshima <yukiyasos@gmail.com>
2021-05-26 00:08:08 +0900

    collection_layout_types

--- ui/collection_layout/yas_ui_collection_layout.cpp
+++ ui/collection_layout/yas_ui_collection_layout.cpp
@@ -10,34 +10,6 @@
 using namespace yas;
 using namespace yas::ui;
 
-#pragma mark - collection_layout::line
-
-bool collection_layout::line::operator==(line const &rhs) const {
-    if (this->new_line_min_offset != rhs.new_line_min_offset) {
-        return false;
-    }
-
-    auto const cell_count = this->cell_sizes.size();
-
-    if (cell_count != rhs.cell_sizes.size()) {
-        return false;
-    }
-
-    auto each = make_fast_each(cell_count);
-    while (yas_each_next(each)) {
-        auto const &idx = yas_each_index(each);
-        if (this->cell_sizes.at(idx) != rhs.cell_sizes.at(idx)) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-bool collection_layout::line::operator!=(line const &rhs) const {
-    return !(*this == rhs);
-}
-
 #pragma mark - collection_layout
 
 collection_layout::collection_layout(args args)
