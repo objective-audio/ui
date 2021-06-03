cf9d4cc70085433ff8236627417148cbbe704c77
Yuki Yasoshima <yukiyasos@gmail.com>
2021-06-03 18:26:26 +0900

    layout

---
+++ ui/layout/yas_ui_layout.cpp
@@ -0,0 +1,44 @@
+//
+//  yas_ui_layout.cpp
+//
+
+#include "yas_ui_layout.h"
+
+#include <ui/yas_ui_layout_dependency.h>
+#include <ui/yas_ui_layout_guide.h>
+
+using namespace yas;
+using namespace yas::ui;
+
+observing::syncable ui::layout(std::shared_ptr<layout_value_source> const &src_guide,
+                               std::shared_ptr<layout_value_target> const &dst_target,
+                               std::function<float(float const &)> &&convert) {
+    return src_guide->observe_layout_value(
+        [weak_target = to_weak(dst_target), convert = std::move(convert)](float const &value) {
+            if (auto const target = weak_target.lock()) {
+                target->set_layout_value(convert(value));
+            }
+        });
+}
+
+observing::syncable ui::layout(std::shared_ptr<layout_point_source> const &src_guide,
+                               std::shared_ptr<layout_point_target> const &dst_target,
+                               std::function<ui::point(ui::point const &)> &&convert) {
+    return src_guide->observe_layout_point(
+        [weak_target = to_weak(dst_target), convert = std::move(convert)](ui::point const &point) {
+            if (auto const target = weak_target.lock()) {
+                target->set_layout_point(convert(point));
+            }
+        });
+}
+
+observing::syncable ui::layout(std::shared_ptr<layout_region_source> const &src_guide,
+                               std::shared_ptr<layout_region_target> const &dst_target,
+                               std::function<ui::region(ui::region const &)> &&convert) {
+    return src_guide->observe_layout_region(
+        [weak_target = to_weak(dst_target), convert = std::move(convert)](ui::region const &region) {
+            if (auto const target = weak_target.lock()) {
+                target->set_layout_region(convert(region));
+            }
+        });
+}
---
+++ ui/layout/yas_ui_layout_dependency.h
@@ -0,0 +1,58 @@
+//
+//  yas_ui_layout_dependency.h
+//
+
+#pragma once
+
+#include <observing/yas_observing_umbrella.h>
+#include <ui/yas_ui_types.h>
+
+namespace yas::ui {
+struct layout_value_source {
+    virtual ~layout_value_source() = default;
+
+    virtual observing::syncable observe_layout_value(std::function<void(float const &)> &&) = 0;
+};
+
+struct layout_point_source {
+    virtual ~layout_point_source() = default;
+
+    virtual observing::syncable observe_layout_point(std::function<void(ui::point const &)> &&) = 0;
+};
+
+struct layout_range_source {
+    virtual ~layout_range_source() = default;
+
+    virtual observing::syncable observe_layout_range(std::function<void(ui::range const &)> &&) = 0;
+};
+
+struct layout_region_source {
+    virtual ~layout_region_source() = default;
+
+    virtual observing::syncable observe_layout_region(std::function<void(ui::region const &)> &&) = 0;
+};
+
+struct layout_value_target {
+    virtual ~layout_value_target() = default;
+
+    virtual void set_layout_value(float const) = 0;
+};
+
+struct layout_point_target {
+    virtual ~layout_point_target() = default;
+
+    virtual void set_layout_point(point const &) = 0;
+};
+
+struct layout_range_target {
+    virtual ~layout_range_target() = default;
+
+    virtual void set_layout_range(range const &) = 0;
+};
+
+struct layout_region_target {
+    virtual ~layout_region_target() = default;
+
+    virtual void set_layout_region(region const &) = 0;
+};
+}  // namespace yas::ui
