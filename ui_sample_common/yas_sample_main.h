fd10f9b9acad04fcbccd46355ed9a7bca49b5d8e
Yuki Yasoshima <yukiyasos@gmail.com>
2021-07-07 23:39:10 +0900

    const

--- ui_sample_common/yas_sample_main.h
+++ ui_sample_common/yas_sample_main.h
@@ -61,9 +61,9 @@ struct main {
     observing::cancellable_ptr _button_canceller = nullptr;
     observing::cancellable_ptr _keyboard_canceller = nullptr;
 
-    std::shared_ptr<ui::node> _render_target_node = ui::node::make_shared();
-    std::shared_ptr<ui::blur> _blur = ui::blur::make_shared();
-    std::shared_ptr<ui::rect_plane> _plane_on_target = ui::rect_plane::make_shared(1);
+    std::shared_ptr<ui::node> const _render_target_node = ui::node::make_shared();
+    std::shared_ptr<ui::blur> const _blur = ui::blur::make_shared();
+    std::shared_ptr<ui::rect_plane> const _plane_on_target = ui::rect_plane::make_shared(1);
     observing::cancellable_ptr _render_target_canceller = nullptr;
 };
 }  // namespace yas::sample
