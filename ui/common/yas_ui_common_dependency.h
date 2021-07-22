ad3089ffd556b43491123750abb7247c8826f10b
Yuki Yasoshima <yukiyasos@gmail.com>
2021-07-22 11:20:55 +0900

    common_scale_factor_interface

---
+++ ui/common/yas_ui_common_dependency.h
@@ -0,0 +1,16 @@
+//
+//  yas_ui_common_dependency.h
+//
+
+#pragma once
+
+#include <observing/yas_observing_umbrella.h>
+
+namespace yas::ui {
+struct common_scale_factor_interface {
+    virtual ~common_scale_factor_interface() = default;
+
+    [[nodiscard]] virtual double scale_factor() const = 0;
+    [[nodiscard]] virtual observing::syncable observe_scale_factor(observing::caller<double>::handler_f &&) = 0;
+};
+}  // namespace yas::ui
