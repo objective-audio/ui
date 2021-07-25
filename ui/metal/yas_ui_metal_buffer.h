7da7f92fc2a89a0ee98a3821f7b7a4cbfcc70278
Yuki Yasoshima <yukiyasos@gmail.com>
2021-07-25 14:34:40 +0900

    metal_buffer

--- ui/metal/yas_ui_metal_buffer.h
+++ ui/metal/yas_ui_metal_buffer.h
@@ -4,8 +4,17 @@
 
 #pragma once
 
+#include <Metal/Metal.h>
+#include <cpp_utils/yas_objc_ptr.h>
 #include <ui/yas_ui_gl_buffer.h>
 
 namespace yas::ui {
-struct metal_buffer final : gl_buffer_interface {};
+struct metal_buffer final : gl_buffer_interface {
+    [[nodiscard]] static std::shared_ptr<metal_buffer> make_shared(id<MTLDevice> const, std::size_t const length);
+
+   private:
+    objc_ptr<id<MTLBuffer>> _raw_buffer;
+
+    metal_buffer(objc_ptr<id<MTLBuffer>> &&);
+};
 }  // namespace yas::ui
--- ui/metal/yas_ui_metal_buffer.mm
+++ ui/metal/yas_ui_metal_buffer.mm
@@ -6,3 +6,12 @@
 
 using namespace yas;
 using namespace yas::ui;
+
+metal_buffer::metal_buffer(objc_ptr<id<MTLBuffer>> &&raw_buffer) : _raw_buffer(std::move(raw_buffer)) {
+}
+
+std::shared_ptr<metal_buffer> metal_buffer::make_shared(id<MTLDevice> const device, std::size_t const length) {
+    auto raw_buffer = objc_ptr_with_move_object([device newBufferWithLength:length
+                                                                    options:MTLResourceOptionCPUCacheModeDefault]);
+    return std::shared_ptr<metal_buffer>(new metal_buffer{std::move(raw_buffer)});
+}
