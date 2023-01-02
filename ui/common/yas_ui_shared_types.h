f04e8bc11241997786de33659ae0d65c6cbec91e
Yuki Yasoshima <yukiyasos@gmail.com>
2023-01-02 22:53:44 +0900

    shared_types final

--- ui/common/yas_ui_shared_types.h
+++ ui/common/yas_ui_shared_types.h
@@ -8,13 +8,13 @@
 
 namespace yas {
 namespace ui {
-    struct vertex2d_t {
+    struct vertex2d_t final {
         simd::float2 position = 0.0f;
         simd::float2 tex_coord = 0.0f;
         simd::float4 color = 1.0f;
     };
 
-    struct uniforms2d_t {
+    struct uniforms2d_t final {
         simd::float4x4 matrix;
         simd::float4 color = 1.0f;
         bool use_mesh_color = false;
