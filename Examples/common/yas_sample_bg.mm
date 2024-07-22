939f78e11ee34c388aa79e1e0260f46ea80dae35
Yuki Yasoshima <yukiyasos@gmail.com>
2024-07-22 22:43:02 +0900

    handle appearance

--- Examples/common/yas_sample_bg.mm
+++ Examples/common/yas_sample_bg.mm
@@ -7,15 +7,28 @@
 using namespace yas;
 using namespace yas::ui;
 
-sample::bg::bg(std::shared_ptr<ui::layout_region_source> const &safe_area_guide) {
-    this->_rect_plane->node()->set_rgb_color({.v = 0.75f});
+sample::bg::bg(std::shared_ptr<ui::view_look> const &view_look) {
+    view_look
+        ->observe_appearance([this](ui::appearance const &appearance) {
+            switch (appearance) {
+                case ui::appearance::normal: {
+                    this->_rect_plane->node()->set_rgb_color({.v = 0.75f});
+                } break;
+                case ui::appearance::dark: {
+                    this->_rect_plane->node()->set_rgb_color({.v = 0.25f});
+                } break;
+            }
+        })
+        .sync()
+        ->add_to(this->_pool);
 
     this->_layout_guide
         ->observe([this](region const &region) { this->rect_plane()->data()->set_rect_position(region, 0); })
         .end()
         ->add_to(this->_pool);
 
-    safe_area_guide->observe_layout_region([this](region const &region) { this->_layout_guide->set_region(region); })
+    view_look->safe_area_layout_guide()
+        ->observe([this](region const &region) { this->_layout_guide->set_region(region); })
         .sync()
         ->add_to(this->_pool);
 }
@@ -24,6 +37,6 @@ std::shared_ptr<rect_plane> const &sample::bg::rect_plane() {
     return this->_rect_plane;
 }
 
-sample::bg_ptr sample::bg::make_shared(std::shared_ptr<ui::layout_region_source> const &safe_area_guide) {
-    return std::shared_ptr<bg>(new bg{safe_area_guide});
+sample::bg_ptr sample::bg::make_shared(std::shared_ptr<ui::view_look> const &view_look) {
+    return std::shared_ptr<bg>(new bg{view_look});
 }
